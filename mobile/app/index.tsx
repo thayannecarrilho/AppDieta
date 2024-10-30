b0ea0cf7d7270914ceec505febf55f3725b35edd
Thay <thayanne.cscarrilho@senacsp.edu.br>
2024-10-29 21:52:22 -0300

    front-end

---
+++ mobile/app/index.tsx
@@ -0,0 +1,63 @@
+import { View, Text, Image, StyleSheet, Pressable} from 'react-native'
+import { colors } from '../constants/colors'
+import { Link } from 'expo-router'
+
+export default function Index(){
+  return(
+    <View style={styles.container}>
+      <Image
+      source={require('../assets/images/logo.png')}/>
+      <Text style={styles.title}>
+        Dieta<Text style={{ color: colors.white }}>.IA</Text>
+      </Text>
+      <Text style={styles.text}>
+        Sua dieta personalizada com inteligência artificial
+      </Text>
+      <Link href="/step" asChild>
+        <Pressable style={styles.button}>
+          <Text style={styles.textButton}>
+            Gerar Dieta
+          </Text>
+        </Pressable>
+      </Link>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: colors.background,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 16
+  },
+  title:{
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: colors.green
+  },
+  text:{
+    fontSize: 16,
+    color: colors.white,
+    width: 240,
+    textAlign: 'center',
+    marginTop: 8,
+    marginBottom: 8
+  }, 
+  button:{
+    backgroundColor: colors.blue,
+    width: '100%',
+    height: 40,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 34,
+  },
+  textButton:{
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
